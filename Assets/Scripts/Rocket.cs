69bf93e0facf207ee6ec692c94287ccef89bc4b4
pepnou <pepnou1@gmail.com>
2020-05-31 15:07:31 +0200

    43

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+
+        bool left = Input.GetKey(KeyCode.Q);
+        bool right = Input.GetKey(KeyCode.D);
+        if(left ^ right)
+        {
+            if(left)
+            {
+
+            }
+
+            if(right)
+            {
+
+            }
+        }
+    }
+}
